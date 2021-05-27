217e797cc0b4d3010224c9c609259d4970fc865e
245893349 <245893349@qq.com>
2021-05-27 13:20:55 +0800

    完成用户注册功能

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,7 +8,12 @@
                 <h5>注册</h5>
             </div>
             <div class="card-body">
+
+                @include('shared._errors')
+
                 <form method="POST" action="{{ route('users.store') }}">
+                    {{ csrf_field() }}
+
                     <div class="form-group">
                         <label for="name">名称：</label>
                         <input type="text" name="name" class="form-control" value="{{ old('name') }}">
@@ -23,7 +28,7 @@
                         <label for="password">密码：</label>
                         <input type="password" name="password" class="form-control" value="{{ old('password') }}">
                     </div>
-                    {{ csrf_field() }}
+
                     <div class="form-group">
                         <label for="password_confirmation">确认密码：</label>
                         <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
